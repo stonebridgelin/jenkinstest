f3098c08a249e89e6f91fd8a25b897c5e35563bc
Administrator <admin@example.com>
2023-07-17 20:58:16 -0700

    Initial commit

---
+++ src/main/java/com/stonebridge/jenkinstest/controller/JenkinsController.java
@@ -0,0 +1,12 @@
+package com.stonebridge.jenkinstest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsController {
+    @GetMapping("/test")
+    public String test() {
+        return "hello jenkins->v2.0.0";
+    }
+}
